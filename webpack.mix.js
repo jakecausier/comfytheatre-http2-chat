d049a6d8d3b0355bc68ba10a3e34e28db7519349
Jake Causier <jake@purplemountmedia.com>
2019-03-26 21:53:34 +0000

    Compiled

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,8 @@
 let mix = require('laravel-mix');
 
 mix.js('src/chat.js', 'public/')
-	 .js('src/chat.js', '/var/www/comfytheatre/html/wp-content/themes/comfytheatre-2019/src/js/')
    .sass('src/site.scss', 'public/')
    .options({
    		processCssUrls: false
    })
-   .copy('node_modules/@fortawesome/fontawesome-free/css/all.css', 'public/fontawesome/fontawesome.css')
-   .copy('node_modules/@fortawesome/fontawesome-free/webfonts/',  'public/webfonts/')
    .version;
